79b13d61f481ac21b03ab46807a03faf5d52cedf
Fiat Bruno <fiatbruno10@gmail.com>
2023-03-02 14:32:11 +0200

    Add sell page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import Header from "@/components/Header"
+import "@/styles/globals.css"
+import { MoralisProvider } from "react-moralis"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            <Header></Header>
+            <Component {...pageProps} />
+        </MoralisProvider>
+    )
 }
