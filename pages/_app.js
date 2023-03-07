be26345ed032f2f0d4470273e7f44d4ddf4028f3
Fiat Bruno <fiatbruno10@gmail.com>
2023-03-07 08:04:01 +0200

    Styling header

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import Header from "@/components/Header"
+import Head from "next/head" 
 import "@/styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 
 export default function App({ Component, pageProps }) {
     return (
-        <MoralisProvider initializeOnMount={false}>
-            <Header></Header>
-            <Component {...pageProps} />
-        </MoralisProvider>
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace to buy and sell your NFTs" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <Header></Header>
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head"
+import Image from "next/image"
+import { Inter } from "next/font/google"
+import styles from "@/styles/Home.module.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta name="description" content="NFT Marketplace to buy and sell your NFTs" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Let's Go
-    </>
-  )
+    return <div className={styles.container}>Let's Go</div>
 }
