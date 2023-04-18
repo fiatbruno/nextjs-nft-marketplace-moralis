4b7c37bf30707f9456d27159744b7aec6ba58fd0
Fiat Bruno <fiatbruno10@gmail.com>
2023-04-18 09:36:06 +0200

    Issue: integrating backend with smartcontracts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import Header from "../components/Header"
-import Head from "next/head" 
+import Head from "next/head"
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
+
+const APP_ID = process.env.NEXT_PUBLIC_APPLICATION_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
+console.log(APP_ID + "      " + SERVER_URL)
 
 export default function App({ Component, pageProps }) {
     return (
@@ -12,7 +17,7 @@ export default function App({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <MoralisProvider initializeOnMount={false}>
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
                 <Header></Header>
                 <Component {...pageProps} />
             </MoralisProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import Head from "next/head"
-import Image from "next/image"
 import { Inter } from "next/font/google"
 import styles from "../styles/Home.module.css"
-
+import { useEvmNativeBalance } from "@moralisweb3/next"
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-    return <div className={styles.container}>Let's Go</div>
+    const address = "0x1..."
+
+    const { data: nativeBalance } = useEvmNativeBalance({ address });
+
+    return (
+        <div>
+            <div className={styles.container}>Let's Go</div>
+            <h3>Wallet: {address}</h3>
+            <h3>Native Balance: {nativeBalance?.balance.ether} ETH</h3>
+        </div>
+        )
 }
